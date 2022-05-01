fa44aa6b1db0809375d8ad1e88fd9440ffabdbde
Adam Kad <44841980+adam-kad@users.noreply.github.com>
2022-05-01 16:04:47 +0300

    Update new_sum_list.py

--- new_sum_list.py
+++ new_sum_list.py
@@ -4,10 +4,12 @@ array_c = [2, 3, 4, 5, 6, 7, 8]
 array_d = [2, 3, 4, 5, 6, 7, 8]
 
 
+result = [a + b + c + d for a, b, c, d in zip(array_a, array_b, array_c, array_d)]
 
-zip_array = zip(array_a, array_b, array_c, array_d) # "Сшиваем" списки, получаем ((1, 3, 5), (2, 4, 6))
-map_array = map(sum, zip_array) # Суммируем элементы в каждом подсписке
-result = list(map_array) # Преобразовываем в list
+
+#zip_array = zip(array_a, array_b, array_c, array_d) # "Сшиваем" списки, получаем
+#map_array = map(sum, zip_array) # Суммируем элементы в каждом подсписке
+#result = list(map_array) # Преобразовываем в list
 
 
 print(result)
